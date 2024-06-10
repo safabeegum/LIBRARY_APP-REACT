a6b9500ca014d52f52974b18300fb15c35978def
safabeegum <pasafabeegum@gmail.com>
2024-06-10 23:07:36 +0530

    Completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="card">
+                <img src="https://img.freepik.com/free-photo/3d-rendering-classic-interior_23-2150943454.jpg?t=st=1718040777~exp=1718044377~hmac=b8a7eaffa04d4eef6ee9ff49d3dc02ab4172bb6f50d62240f210738fa38d4c2d&w=900" class="card-img-top" alt="..."></img>
+  
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
