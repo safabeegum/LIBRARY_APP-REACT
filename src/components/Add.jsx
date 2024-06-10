8c7d164764a3c59ae97f8efe58b5ad13531d055a
safabeegum <pasafabeegum@gmail.com>
2024-06-10 23:45:07 +0530

    Completed

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+import axios from 'axios'
 
 const Add = () => {
+    const[data,setData]=useState(
+        {
+            "name" : "",
+        "rollno":"",
+        "admnno":"",
+        "college":""
+        }
+    )
+
+    const inputHandler=(event) => {
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue=() => {
+        console.log(data)
+        axios.post("",data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Successfully Added")
+                } else {
+                    alert("Error")
+                }
+            }
+        ).catch().finally()
+    }
   return (
     <div>
+        <Navbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -14,7 +43,7 @@ const Add = () => {
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="" className="form-label">CATEGORY</label>
-                            <select name="name" id="" className="form-control" value={data.category} onChange={inputHandler}>
+                            <select name="category" id="" className="form-control" value={data.category} onChange={inputHandler}>
                                 <option value="FANTASY">FANTASY</option>
                                 <option value="ROMANCE">ROMANCE</option>
                                 <option value="THRILLER">THRILLER</option>
--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+import axios from 'axios'
 
 const Delete = () => {
+    const[data,setData]=useState(
+        {
+            "name":""
+        }
+    )
+
+    const inputHandler=(event)=>{
+        setData({...data, [event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(data)
+        axios.post("",data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Successfully Added")
+                } else {
+                    alert("Error")
+                }
+            }
+        ).catch().finally()
+    }
   return (
     <div>
+        <Navbar/>
         <div className="container">
             <div className="row g-3">
                 <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
