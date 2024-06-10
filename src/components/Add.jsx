a93432778b34ffb3a94b328a8db91ecf0c364359
safabeegum <pasafabeegum@gmail.com>
2024-06-10 23:12:05 +0530

    Completed

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -3,7 +3,42 @@ import React from 'react'
 const Add = () => {
   return (
     <div>
-        .container>.
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">NAME</label>
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">CATEGORY</label>
+                            <select name="name" id="" className="form-control" value={data.category} onChange={inputHandler}>
+                                <option value="FANTASY">FANTASY</option>
+                                <option value="ROMANCE">ROMANCE</option>
+                                <option value="THRILLER">THRILLER</option>
+                                <option value="HORROR">HORROR</option>
+                            </select>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">AUTHOR</label>
+                            <input type="text" className="form-control" name='author' value={data.author} onChange={inputHandler}/>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PRICE</label>
+                            <input type="text" className="form-control" name='price' value={data.price} onChange={inputHandler}/>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success" onClick={readValue}>SUCCESS</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
